af428b41365c03ff36d23ce6ddd3b8b2cac15e83
Jihyun Bang <01066303821jh@gmail.com>
2023-08-22 17:23:13 +0900

    useState vs useRef 차이

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-// useEffect & Clean up - Timer
-import React, { useState } from 'react';
-import Timer from './component/Timer.js'
+import { useState, useRef } from "react"
 
 export default function App() {
-  const [showTimer, setShowTimer] = useState(false)
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+
+  // {current : 0} => Ref 는 하나의 객체(object)이다. ref 안의 값을 보고싶다면 ref.current 를 사용할것!
+  console.log(countRef);
+
+  const increaseCountState = () => {
+    setCount(count + 1);
+  }
+
+  const increaseCountRef = () => {
+    countRef.current = countRef.current + 1;
+  }
 
   return (
     <div>
-      {showTimer && <Timer />}
-      <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
+      <p>State : {count}</p>
+      <p>Ref : {countRef.current}</p>
+      <button onClick={increaseCountState}>State 올리기</button>
+      {/* 위에 State 올리기 버튼을 누르면 state 가 변경되므로 누를때마다 랜더링이되지만, Ref 같은 경우는 재 랜더링이 되지 않는다.
+      하지만 ref 버튼도 누를때마다 값은 변경되고있다.
+      이후에 state 버튼을 눌러 재 랜더링을 하게되면, ref 의 값이 변해있다. */}
+      <button onClick={increaseCountRef}>Ref 올리기</button>
     </div>
   )
 }
