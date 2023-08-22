adb0ca3c39b0319e42583aa9a331ddca5fda0b60
Jihyun Bang <01066303821jh@gmail.com>
2023-08-22 18:12:16 +0900

    useRef vs 변수 차이

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import { useState, useRef } from "react"
+import { useRef, useState } from "react"
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const countRef = useRef(0);
+  const [renderer, setRenderer] = useState(0);
+  const countRef = useRef(0)
+  let countVar = 0;
 
-  // {current : 0} => Ref 는 하나의 객체(object)이다. ref 안의 값을 보고싶다면 ref.current 를 사용할것!
-  console.log(countRef);
-
-  const increaseCountState = () => {
-    setCount(count + 1);
+  const increaseRef = () => {
+    countRef.current = countRef.current + 1
+    console.log('ref: ', countRef.current + 1)
+  }
+  const increaseVar = () => {
+    countVar = countVar + 1
+    console.log('Var: ', countVar)
+  }
+  const doRendering = () => {
+    setRenderer(renderer + 1);
   }
 
-  const increaseCountRef = () => {
-    countRef.current = countRef.current + 1;
+  const printResults = () => {
+    console.log(`ref : ${countRef.current}, var : ${countVar}`)
   }
 
   return (
     <div>
-      <p>State : {count}</p>
       <p>Ref : {countRef.current}</p>
-      <button onClick={increaseCountState}>State 올리기</button>
-      {/* 위에 State 올리기 버튼을 누르면 state 가 변경되므로 누를때마다 랜더링이되지만, Ref 같은 경우는 재 랜더링이 되지 않는다.
-      하지만 ref 버튼도 누를때마다 값은 변경되고있다.
-      이후에 state 버튼을 눌러 재 랜더링을 하게되면, ref 의 값이 변해있다. */}
-      <button onClick={increaseCountRef}>Ref 올리기</button>
+      <p>Var : {countVar}</p>
+      {/* Ref, Var 버튼 똑같이 클릭 후, 랜더링 버튼을 누르면 Ref 만 변경되어있고, Var 은 그대로이다.
+      이유는, 컴포넌트가 다시 랜더링될때 App함수가 다시 처음부터 시작되고, 그 안에 있는 변수들을 그때마다 계속 초기화가된다.
+      하지만 Ref는, 아무리 컴포넌트가 랜더링이되도 값을 유지합니다.
+      ref 의 값은 이 컴포넌트가 브라우저가 Mounting 된 시점부터 unMounting 될때까지 같은 값을 계속 유지할 수 있다는 뜻입니다. */}
+      <button onClick={doRendering}>랜더링!</button>
+      <button onClick={increaseRef}>Ref 올려</button>
+      <button onClick={increaseVar}>Var 올려</button>
+      <button onClick={printResults}>Ref Var 값 출력</button>
     </div>
   )
 }
