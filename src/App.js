22e3a55d2ed9a2957f5cab46873680ae76625b2e
Jihyun Bang <01066303821jh@gmail.com>
2023-08-22 14:11:04 +0900

    react -useEffect연습(count 만들기)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// useEffect 사용하여 Counter 만들기
+// 왜 처음에 mount 될때 console.log('랜더링중') 두번찍힐까..?
+
+import React, { useEffect, useState } from 'react';
+
+export default function App () {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
+  const handleCountUpdate = () => {
+    setCount(count + 1);
+  };
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
+  // 랜더링이 될때마다 실행됨- 랜더링 이후
+  useEffect(() => {
+    console.log('랜더링중');
+  });
+
+  // 마운팅 + [ count ] 변경될때만 실행.
+  useEffect(() => {
+    console.log('count 가 변경 될때만 실행');
+  },[count]);
+
+  // 마운팅 + [ name ] 변경될때만 실행.
+  useEffect(() => {
+    console.log('name 가 변경 될때만 실행');
+  },[name]);
+
+  // 맨처음 랜더링 될때만 실행
+  useEffect(() => {
+    console.log('처음 랜더링 될때만 실행됩니다');
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleCountUpdate}>Update</button>
+      <span>{count}</span>
+      <input type="text" onChange={handleInputChange} ></input>
+      <span>{name}</span>
+
     </div>
   );
 }
 
-export default App;
