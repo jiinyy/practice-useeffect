1461178d96d319ef435c2b71e732790170c4ae99
Jihyun Bang <01066303821jh@gmail.com>
2023-08-22 18:41:03 +0900

    useState + useEffect 무한루프(재랜더링 벗어나기) => useRef 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function App() {
   const [count, setCount] = useState(1);
-  const [renderCount, setRenderCount] = useState(1);
+  const renderCount = useRef(0);
 
-  // 무한루프에 갇힘.. => setCount => Rendering 하는데 그 안에서 또 setRenderCount => 랜더링 또랜더링.....
+  // Ref 는 re-rendering 을 시키지 않으므로 무한루프에 빠지지 않게된다.
+  // 즉, useRef 는 변화는 감지해야되지만, 변화가 랜더링을 발생시키면 안되는 어떤 값을 다룰때 유용하다.
   useEffect(() => {
-    console.log(setRenderCount(renderCount + 1))
+    renderCount.current = renderCount.current + 1;
+    console.log('랜더링 수: ', renderCount.current)
   })
 
   return (
