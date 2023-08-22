0e5d5f025a402ec0688dc23c320d943cfbb1b7cd
Jihyun Bang <01066303821jh@gmail.com>
2023-08-22 18:59:40 +0900

    useRef #2 Dom 요소접근 (input focus())

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef } from "react"
 
 export default function App() {
-  const [count, setCount] = useState(1);
-  const renderCount = useRef(0);
+  const inputRef = useRef()
 
-  // Ref 는 re-rendering 을 시키지 않으므로 무한루프에 빠지지 않게된다.
-  // 즉, useRef 는 변화는 감지해야되지만, 변화가 랜더링을 발생시키면 안되는 어떤 값을 다룰때 유용하다.
   useEffect(() => {
-    renderCount.current = renderCount.current + 1;
-    console.log('랜더링 수: ', renderCount.current)
-  })
+    // console.log(inputRef)
+    inputRef.current.focus();
+  }, [])
+
+  const login = () => {
+    alert(`환영합니다 ${inputRef.current.value}!`)
+    inputRef.current.focus();
+  }
 
   return (
     <div>
-      <p>Count : {count}</p>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <input type="text" placeholder="username" ref={inputRef}></input>
+      <button onClick={login}>로그인</button>
     </div>
   )
 }
